22060e60b08ee249b5d1be53e327993c9a033067
unknown <v.bridet98@gmail.com>
2025-02-19 00:25:18 +0100

    Initial commit

---
+++ push_to_github.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "D:\Vincent\- Informatique -\Echecs OpenAi\Caissa"   REM Change le répertoire vers ton projet
+git add .
+git commit -m "Mise à jour automatique"
+git push origin main
